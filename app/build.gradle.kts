b4015859668e76ddc80a3342e07fe936d555a612
calt-laboratory <christoph_alt@posteo.de>
2026-02-28 18:58:21 +0100

    chore(gradle): added gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.gson)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui)
