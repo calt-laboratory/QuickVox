bd7868c5d388a0edc4bc597fe7b01f71b73060dd
calt-laboratory <christoph_alt@posteo.de>
2026-03-01 16:52:21 +0100

    feat(voice grocery screen): implemented screen for voice grocery list

--- app/src/main/java/com/caltlab/quickvox/navigation/Screen.kt
+++ app/src/main/java/com/caltlab/quickvox/navigation/Screen.kt
@@ -2,6 +2,7 @@ package com.caltlab.quickvox.navigation
 
 sealed class Screen(val route: String) {
     object MainScreen : Screen("main")
+    object GroceryVoiceListScreen : Screen("voice_grocery_list")
     object GroceryListScreen : Screen("grocery_list")
     object NotesScreen : Screen("notes")
 }
